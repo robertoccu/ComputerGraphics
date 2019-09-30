e58bf6d749de5a32436d8c4629285b77691125b1
robertoccu <ccucraft@gmail.com>
2019-09-30 09:06:08 +0200

    RGB class header

---
+++ maths/RGB.h
@@ -0,0 +1,31 @@
+/**
+ * RGB
+ */
+
+#ifndef RGB_H
+#define RGB_H
+
+#include <iostream>
+#include <list>
+
+#endif //RGB_H
+
+class RGB {
+private:
+    double value[3];
+public:
+    RGB();
+    RGB(double red, double green, double blue);
+
+    ~RGB();
+
+    /* Operations */
+    static RGB average(const std::list<RGB> &rgb);
+
+    /* Operator overload */
+    RGB operator+(const RGB &rgb) const;
+    RGB operator+=(const RGB &rgb);
+
+    /* Screen output */
+    friend std::ostream &operator<<(std::ostream &os, const RGB &rgb);
+};
