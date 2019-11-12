1a64705dd7580553199c407d66763cf829640a8c
sherrero96 <sergio.herrero.barco@mail.com>
2019-11-12 01:33:38 +0100

    Parallelism depending on the number of cores made. Missing proof

--- render/tracer.cpp
+++ render/tracer.cpp
@@ -2,7 +2,11 @@
 // Created by Sergio on 08/11/2019.
 //
 
+
 #include "tracer.h"
+
+// Atomic variable that contains the number of rows that are already finished.
+std::atomic<int> tracer::threads_progress(0);
 
 /**
  * Main method that calculates the image of the scene.
@@ -12,36 +16,37 @@
  * @return image of the scene
  */
 Image tracer::ray_tracer(const Scene &scene, int paths_per_pixel) {
-    std::random_device rd;
-    std::mt19937 mt(rd());
-    std::uniform_real_distribution<float> dist(0.0, 1.0);
-
     // Image creation
     Image image("RayTracer", 1, scene.getScreen().getPixelsColumn(), scene.getScreen().getPixelsRow(),
             65535);
-    RGB colors[paths_per_pixel];
-    float percentage;
-    Vector pixel;
-    Ray ray;
 
-    for(int row = 0; row < scene.getScreen().getPixelsRow(); row++){
-        // The percentage is calculated by rows
-        percentage =  row*100 / scene.getScreen().getPixelsRow();
-        cout<< '\r' << (int) percentage <<" %";
-        for(int column = 0; column < scene.getScreen().getPixelsColumn(); column++){
-            for(int path = 0; path != paths_per_pixel; path++){
-                // Obtain the pixel in world coordinates with a random offset
-                pixel = scene.getScreen().get_pixel(row, column, dist(mt));
-                // Create the ray
-                ray = ray.rayFromPoints(scene.getCamera().getPosition(), pixel);
-                // Obtain the color result of the intersection and save in the vector
-                 colors[path] = ray_tracer(ray, scene);
-            }
-            // Set the color result in the image
-            image.setPixel(row, column, RGB::average_colors(colors, paths_per_pixel));
-        }
+    /* Obtain the max number of cores
+     * If you want to set the number of cores, change the variable and leave it at a fixed value */
+    unsigned number_Cores = std::thread::hardware_concurrency();
+    // We check if the rows can be divided by the cores.
+    // If it is not divisible, there will be some row that is not generated. Program will stop.
+    if((scene.getScreen().getPixelsRow() % number_Cores) != 0){
+        cerr<<"You can't divide the number of columns by the cores that you have"<<endl;
+        cerr<<"Change the number of cores OR the resolution_Y in scene"<<endl;
+        exit(1);
     }
-    cout<< "\r" <<"100 %"<<endl;
+
+    // Obtain the number of rows for worker
+    unsigned int rows_for_worker = scene.getScreen().getPixelsRow() / number_Cores;
+    // Create a vector of threads
+    std::thread workers[number_Cores + 1];
+    // We launched all the workers
+    for(int i = 0; i < number_Cores; i++){
+        workers[i] = std::thread(&worker_tracer, i * rows_for_worker, (i * rows_for_worker) + rows_for_worker - 1,
+                ref(image), ref(scene), paths_per_pixel);
+    }
+    workers[number_Cores] = std::thread(&show_progress, scene.getScreen().getPixelsRow());
+    // We hope that it will be finished.
+    for(int i = 0; i < number_Cores; i++){
+        workers[i].join();
+    }
+    workers[number_Cores].join();
+    cout<<"\n100 %"<<endl;
     return image;
 }
 
@@ -60,5 +65,51 @@ RGB tracer::ray_tracer(const Ray &ray, const Scene &scene) {
         return collision_object->getEmision();  // Now return the color emission
     }else{
         return RGB(0, 0, 0);    // Return black
+    }
+}
+
+/**
+ * Each worker has the task of completing x rows of the image.
+ * @param pixel_row_initial Row in which the worker must start working
+ * @param pixel_row_final Row in which the worker must end working, included
+ * @param image Image to store the result
+ * @param scene Scene with the all parameters
+ */
+void tracer::worker_tracer(unsigned int pixel_row_initial, unsigned int pixel_row_final, Image &image,
+        const Scene &scene, int paths_per_pixel){
+
+    RGB colors[paths_per_pixel];
+    Vector pixel;
+    Ray ray;
+    for(unsigned int row = pixel_row_initial; row <= pixel_row_final; row++){
+        threads_progress++;
+        for(int column = 0; column < scene.getScreen().getPixelsColumn(); column++){
+            for(int path = 0; path != paths_per_pixel; path++){
+                // Obtain the pixel in world coordinates with a random offset
+                pixel = scene.getScreen().get_pixel(row, column);
+                // Create the ray
+                ray = ray.rayFromPoints(scene.getCamera().getPosition(), pixel);
+                // Obtain the color result of the intersection and save in the vector
+                colors[path] = ray_tracer(ray, scene);
+            }
+            // Set the color result in the image
+            image.setPixel(row, column, RGB::average_colors(colors, paths_per_pixel));
+        }
+    }
+}
+
+/**
+ *
+ * @param rows
+ */
+void tracer::show_progress(const unsigned int rows){
+    int progress = 0;
+    float percentage;
+    while(progress < rows - 1){
+        // The percentage is calculated by rows
+        percentage =  progress * 100.0 / (float) rows;
+        cout<< '\r' << (int) percentage <<" %";
+        progress = threads_progress.load(std::memory_order_relaxed);
+        //std::this_thread::sleep_for (std::chrono::seconds(1));
     }
 }
--- render/tracer.h
+++ render/tracer.h
@@ -6,6 +6,8 @@
 #define COMPUTERGRAPHICS_TRACER_H
 
 #include <random>
+#include <thread>
+#include <atomic>
 #include "../imaging/Image.h"
 #include "Scene.h"
 
@@ -13,7 +15,13 @@ class tracer {
 public:
     static Image ray_tracer(const Scene &scene, int paths_per_pixel);
 private:
+    static void worker_tracer(unsigned int pixel_row_initial, unsigned int pixel_row_final, Image &image,
+            const Scene &scene, int paths_per_pixel);
     static RGB ray_tracer(const Ray &ray, const Scene &scene);
+
+private:
+    static std::atomic<int> threads_progress;
+    static void show_progress(const unsigned int rows);
 };
 
 
